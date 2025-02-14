5af6d424ad14b1dc477d348d1e1a85b71a3754cf
Unknownbox <34061369+perpect@users.noreply.github.com>
2025-02-14 01:04:16 +0000

    modified:   1st Webgame/map.js

--- 1st Webgame/map.js
+++ 1st Webgame/map.js
@@ -10,7 +10,7 @@ class MapTile{
 
     draw(tileX, tileY, playerX, playerY, size){
         const SQRT3 = 1.732;
-        let x = tileX * SQRT3 / 2 - tileY * size * SQRT3 / 2 - playerX;
+        let x = tileX * size * SQRT3 / 2 - tileY * size * SQRT3 / 2 - playerX;
         let y = - tileY * size * (1 + SQRT3) / 2 - playerY;
 
         ctx.fillStyle = "#000000";
