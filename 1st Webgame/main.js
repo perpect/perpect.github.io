e6ecd71f7c01f180946f9573d854dc9db49c3947
Unknownbox <34061369+perpect@users.noreply.github.com>
2025-02-14 00:54:56 +0000

    renamed:    main.html -> 1st Webgame/main.html 	new file:   1st Webgame/main.js 	new file:   1st Webgame/map.js 	deleted:    main.js

---
+++ 1st Webgame/main.js
@@ -0,0 +1,19 @@
+const canvas = document.getElementById("mainCanv");
+const ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+
+var a = MapTile(0);
+
+function update() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    a.draw(x, y, 0, 0);
+    x += dx;
+    y += dy;
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+setInterval(update, 10);
---
+++ 1st Webgame/map.js
@@ -0,0 +1,30 @@
+const Type = {
+    PLAIN : 0
+};
+Object.freeze(Type);
+
+class MapTile{
+    constructor(type) {
+        this.type = type;
+    }
+
+    draw(tileX, tileY, playerX, playerY, size){
+        const SQRT3 = 1.732;
+        let x = tileX * SQRT3 / 2 - tileY * size * SQRT3 / 2 - playerX;
+        let y = - tileY * size * (1 + SQRT3) / 2 - playerY;
+
+        ctx.fillStyle = "#000000";
+        ctx.lineWidth = 3;
+        ctx.beginPath();
+        ctx.moveTo(x, y + size / 2);
+        ctx.lineTo(x + size * SQRT3 / 2, y + size / 2);
+        ctx.lineTo(x + size * SQRT3 / 2, y - size / 2);
+        ctx.lineTo(x, y - size / 2);
+        ctx.lineTo(x - size * SQRT3 / 2, y - size / 2);
+        ctx.lineTo(x - size * SQRT3 / 2, y + size / 2);
+        ctx.lineTo(x, y + size / 2);
+        ctx.fill();
+        ctx.stroke();
+        ctx.closePath();
+    }
+}
