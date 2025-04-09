29246f61ebc1a13a9b9f10248bb2d5ea2f67f98d
Unknownbox <34061369+perpect@users.noreply.github.com>
2025-04-09 12:28:02 +0000

    Lexer 일부 구현

--- CompilerTest/Scan.js
+++ CompilerTest/Scan.js
@@ -5,40 +5,6 @@ class Token{
     }
 }
 
-var scanIdx;
-var sourceCode;
-
-function scan(ori) {
-    let result = [];
-    sourceCode = ori;
-    sourceCode += '\0';
-    scanIdx = 0;
-    while (sourceCode[scanIdx] != '\0') {
-        switch (getCharType(sourceCode[scanIdx])) {
-            case CharType.WhiteSpace:
-                scanIdx += 1;
-                break;
-            case CharType.NumberLiteral:
-                result.push(scanNumberLiteral());
-                break;
-            case CharType.StringLiteral:
-                result.push(scanStringLiteral());
-                break;
-            case CharType.IdentifierAndKeyword:
-                result.push(scanIdentifierAndKeyword());
-                break;
-            case CharType.OperatorAndPunctuator:
-                result.push(scanOperatorAndPunctuator());
-                break;
-            default:
-                console.log(sourceCode[scanIdx] + " : 사용할 수 없는 문자입니다.");
-                return undefined;
-        }
-    }
-    result.push(kind.EndOfToken);
-    return result;
-}
-
 var CharType = {
     Unknown : 0,
     WhiteSpace : 1,
@@ -49,95 +15,129 @@ var CharType = {
 };
 Object.freeze(CharType);
 
-function getCharType(c) {
-    if (' ' == c || '\t' == c || '\r' == c || '\n' == c)
-        return CharType.WhiteSpace;
-    if ('0' <= c && c <= '9')
-        return CharType.NumberLiteral;
-    if (c == '"' || c == "'")
-        return CharType.StringLiteral;
-    if ('a' <= c && c <= 'z' || 'A' <= c && c <= 'Z')
-        return CharType.IdentifierAndKeyword;
-    if (33 <= c && c <= 47 ||
-        58 <= c && c <= 64 ||
-        91 <= c && c <= 126)
-        return CharType.OperatorAndPunctuator;
-    return CharType.Unknown;
-}
+/*var CharRegex = {
+    Unknown : 0,
+    WhiteSpace : new RegExp("[ \t\n\v\f\r]*"),
+    NumberLiteral : new RegExp("(0|\d+)(\.\d+)?((e|E)(\-|\+)?\d+)?"),
+    StringLiteral : new RegExp("\"([^\\\"]|(\\([\\\"bfnrt]|u\d{4})))*\""),
+    IdentifierAndKeyword : new RegExp("[ \t\n\v\f\r]*"),
+    OperatorAndPunctuator : 5
+};
+Object.freeze(CharType);*/
 
-function isCharType(c, type) {
-    switch (type) {
-        case CharType.NumberLiteral:
-            return '0' <= c && c <= '9';
-        case CharType.StringLiteral:
-            return 32 <= c && c <= 126 && c != '"' && c!='"';
-        case CharType.IdentifierAndKeyword:
-            return '0' <= c && c <= '9' ||
-                'a' <= c && c <= 'z' ||
-                'A' <= c && c <= 'Z';
-        case CharType.OperatorAndPunctuator:
-            return 33 <= c && c <= 47 && c != '"' && c != "'" ||
-                58 <= c && c <= 64 ||
-                91 <= c && c <= 96 ||
-                123 <= c && c <= 126;
-        default:
-            return false;
+var scanIdx;
+var scanLine;
+var scanIdxInLine;
+var sourceCode;
+
+function scan(source) {
+    sourceCode = source;
+    scanIdx = 0;
+    scanLine = 0;
+    scanIdxInLine = 0;
+
+    let result = [];
+    let token;
+
+    while (scanIdx < sourceCode.length) {
+        const char = sourceCode[scanIdx];
+
+        if (char === '"' || char === "'") {
+            token = stringTokenizer(sourceCode);
+        } else if (/\s/.test(char)) {
+            token = whiteSpaceTokenizer(sourceCode);
+        } else if (/[a-zA-Z_]/.test(char)) {
+            token = identifierTokenizer(sourceCode);
+        } else {
+            // 추후 operatorTokenizer나 numberTokenizer로 분리
+            token = new Token(Kind.Unknown, char);
+        }
+
+        if (token) result.push(token);
+        scanIdx++;
+        scanIdxInLine++;
     }
+    result.push(new Token(Kind.EndOfToken, null));
+    for (let token of result) {
+        console.log(`[${token.kind}] "${token.content}"`);
+    }
+    return result;
 }
 
-function scanNumberLiteral() {
-    let string;
-    if (sourceCode[scanIdx] == '.') {
-        string += sourceCode[scanIdx];
+function stringTokenizer(sourceCode) {
+    let char;
+    let content = "";
+    let escape = false;
+    let startSymbol = sourceCode[scanIdx];
+    while(scanIdx < sourceCode.length){
         scanIdx += 1;
-        while (isCharType(sourceCode[scanIdx], CharType.NumberLiteral)) {
-            string += sourceCode[scanIdx];
-            scanIdx += 1;
+        scanIdxInLine += 1;
+        char = sourceCode[scanIdx];
+        if (escape){
+            switch (char){
+                case "b":
+                    content = content.slice(-1);
+                    break;
+                case "n":
+                    content += "\n";
+                    break;
+                case "t":
+                    content += "    ";
+                    break;
+                default:
+                    content += char;
+            }
+            escape = false;
+        }
+        else if (char == "\\")
+            escape = true;
+        else if(char == startSymbol)
+            return new Token(Kind.StringLiteral, content);
+        else
+            content += char;
+    }
+    console.log(scanLine + ":" + scanIdxInLine + "종결되지 않은 문자열 리터럴입니다.");
+    return null;
+}
+
+function whiteSpaceTokenizer(sourceCode) {
+    let content = "";
+    let char;
+    while (scanIdx < sourceCode.length) {
+        char = sourceCode[scanIdx];
+        if (char === " " || char === "\t" || char === "\r") {
+            content += char;
+            scanIdx++;
+            scanIdxInLine++;
+        } else if (char === "\n"){
+            scanIdx++;
+            scanIdxInLine = 0;
+            scanLine++;
+        }
+        else {
+            scanIdx--;
+            scanIdxInLine--;
+            break;
         }
     }
-    return new Token(Kind.NumberLiteral, string);
+    return new Token(Kind.WhiteSpace, content);
 }
 
-function scanStringLiteral() {
-    let string;
-    scanIdx += 1;
-    while (isCharType(sourceCode[scanIdx], CharType.StringLiteral)) {
-        string += sourceCode[scanIdx];
-        scanIdx += 1;
-    }
-    if (sourceCode[scanIdx] != '\'') {
-        console.log("문자열의 종료 문자가 없습니다.");
-        return undefined;
-    }
-    scanIdx += 1;
-    return new Token(Kind.StringLiteral, string);
-}
+function identifierTokenizer(sourceCode) {
+    let content = "";
+    let char = sourceCode[scanIdx];
 
-function scanIdentifierAndKeyword() {
-    let string;
-    while (isCharType(sourceCode[scanIdx], CharType.IdentifierAndKeyword)) {
-        string += sourceCode[scanIdx];
-        scanIdx += 1;
+    while (scanIdx < sourceCode.length) {
+        char = sourceCode[scanIdx];
+        if (/[a-zA-Z0-9_]/.test(char)) {
+            content += char;
+            scanIdx++;
+            scanIdxInLine++;
+        } else {
+            scanIdx--;
+            scanIdxInLine--;
+            break;
+        }
     }
-    kind = toKind(string);
-    if (kind == Kind.Unknown)
-        kind = Kind.Identifier;
-    return new Token(kind, string);
-}
-
-function scanOperatorAndPunctuator() {
-    let string;
-    while (isCharType(sourceCode[scanIdx], CharType.OperatorAndPunctuator)) {
-        stirng += sourceCode[scanIdx];
-        scanIdx += 1;
-    }
-    while (string.empty() == false && toKind(string) == Kind.Unknown) {
-        string.pop_back();
-        scanIdx--;
-    }
-    if (string.empty()) {
-        console.log(sourceCode[scanIdx], "사용할 수 없는 문자입니다.");
-        return undefined;
-    }
-    return new Token(toKind(string), string);
+    return new Token(Kind.Identifier, content);
 }
