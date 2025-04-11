a0b4e90a0b704ae968e431eaa2ed0bd4d795455d
Unknownbox <34061369+perpect@users.noreply.github.com>
2025-04-11 01:35:19 +0000

    Scanner 거의 완성 -> 연산자 및 숫자 구현

--- CompilerTest/Kind.js
+++ CompilerTest/Kind.js
@@ -1,95 +1,12 @@
-var Kind = {
-    Unknown : 0,
-    EndOfToken : 1,
-    NullLiteral : 2,
-    TrueLiteral : 3,
-    FalseLiteral : 4,
-    NumberLiteral : 5,
-    StringLiteral : 6,
-    Identifier : 7,
-
-    Variable : 8,
-    For : 9,
-    Break : 10,
-    Continue : 11,
-    If : 12,
-    Elif : 13,
-    Else : 14,
-    Print : 15,
-    PrintLine : 16,
-
-    LogicalAnd : 17,
-    LogicalOr : 18,
-    Assignment : 19,
-    Add : 20,
-    Subtract : 21,
-    Multiply : 22,
-    Divide : 23,
-    Modulo : 24,
-    Equal : 25,
-    NotEqual : 26,
-    LessThan : 27,
-    GreaterThan : 28,
-    LessOrEqual : 29,
-    GreaterOrEqual : 30,
-
-    Comma : 31,
-    Colon : 32,
-    Semicolon : 33,
-    LeftParen : 34,
-    RightParen : 35,
-    LeftBrace : 36,
-    RightBrace : 37,
-    LeftBraket : 38,
-    RightBraket : 39
+const Kind = {
+    WhiteSpace: "WhiteSpace",
+    NumberLiteral: "NumberLiteral",
+    StringLiteral: "StringLiteral",
+    Identifier: "Identifier",
+    Keyword: "Keyword",
+    OperatorAndPunctuator: "OperatorAndPunctuator",
+    EndOfToken: "EndOfToken"
 };
-Object.freeze(Kind);
-
-var Kind = [
-    "Unknown",
-    "EndOfToken",
-    "NullLiteral",
-    "TrueLiteral",
-    "FalseLiteral",
-    "NumberLiteral",
-    "StringLiteral",
-    "Identifier",
-
-    "Variable",
-    "For",
-    "Break",
-    "Continue",
-    "If",
-    "Elif",
-    "Else",
-    "Print",
-    "PrintLine",
-
-    "LogicalAnd",
-    "LogicalOr",
-    "Assignment",
-    "Add",
-    "Subtract",
-    "Multiply",
-    "Divide",
-    "Modulo",
-    "Equal",
-    "NotEqual",
-    "LessThan",
-    "GreaterThan",
-    "LessOrEqual",
-    "GreaterOrEqual",
-
-    "Comma",
-    "Colon",
-    "Semicolon",
-    "LeftParen",
-    "RightParen",
-    "LeftBrace",
-    "RightBrace",
-    "LeftBraket",
-    "RightBraket"
-];
 Object.freeze(Kind);
 
 function toKind(string) {
--- CompilerTest/Scan.js
+++ CompilerTest/Scan.js
@@ -5,25 +5,18 @@ class Token{
     }
 }
 
-var CharType = {
-    Unknown : 0,
-    WhiteSpace : 1,
-    NumberLiteral : 2,
-    StringLiteral : 3,
-    IdentifierAndKeyword : 4,
-    OperatorAndPunctuator : 5
-};
-Object.freeze(CharType);
+function isDigit(c){
+    return "0" <= c && c <= "9";
+}
 
-/*var CharRegex = {
-    Unknown : 0,
-    WhiteSpace : new RegExp("[ \t\n\v\f\r]*"),
-    NumberLiteral : new RegExp("(0|\d+)(\.\d+)?((e|E)(\-|\+)?\d+)?"),
-    StringLiteral : new RegExp("\"([^\\\"]|(\\([\\\"bfnrt]|u\d{4})))*\""),
-    IdentifierAndKeyword : new RegExp("[ \t\n\v\f\r]*"),
-    OperatorAndPunctuator : 5
-};
-Object.freeze(CharType);*/
+function tokenizerError(content){
+    console.error("토큰화 과정 오류:" + content + ":" + scanLine + ":" + scanIdxInLine);
+}
+
+function nextIdx(){
+    scanIdx++;
+    scanIdxInLine++;
+}
 
 var scanIdx;
 var scanLine;
@@ -33,7 +26,7 @@ var sourceCode;
 function scan(source) {
     sourceCode = source;
     scanIdx = 0;
-    scanLine = 0;
+    scanLine = 1;
     scanIdxInLine = 0;
 
     let result = [];
@@ -42,20 +35,23 @@ function scan(source) {
     while (scanIdx < sourceCode.length) {
         const char = sourceCode[scanIdx];
 
-        if (char === '"' || char === "'") {
+        if (char == '"' || char == "'") {
             token = stringTokenizer(sourceCode);
-        } else if (/\s/.test(char)) {
+        } else if (/[\t\r\n ]/.test(char)) {
+            console.log(char);
             token = whiteSpaceTokenizer(sourceCode);
-        } else if (/[a-zA-Z_]/.test(char)) {
+        } else if ("=+\-*/!<>():;{},.".includes(char)) {
+            token = operatorTokenizer(sourceCode);
+        } else if (isDigit(char)) {
+            token = numberTokenizer(sourceCode);
+        } else if (isIdentifierPart(char)) {
             token = identifierTokenizer(sourceCode);
-        } else {
-            // 추후 operatorTokenizer나 numberTokenizer로 분리
+        } else {    
+            nextIdx();
             token = new Token(Kind.Unknown, char);
         }
 
         if (token) result.push(token);
-        scanIdx++;
-        scanIdxInLine++;
     }
     result.push(new Token(Kind.EndOfToken, null));
     for (let token of result) {
@@ -70,8 +66,7 @@ function stringTokenizer(sourceCode) {
     let escape = false;
     let startSymbol = sourceCode[scanIdx];
     while(scanIdx < sourceCode.length){
-        scanIdx += 1;
-        scanIdxInLine += 1;
+        nextIdx();
         char = sourceCode[scanIdx];
         if (escape){
             switch (char){
@@ -89,55 +84,112 @@ function stringTokenizer(sourceCode) {
             }
             escape = false;
         }
-        else if (char == "\\")
+        else if(char == "\\")
             escape = true;
-        else if(char == startSymbol)
+        else if(char == startSymbol){
+            nextIdx();
             return new Token(Kind.StringLiteral, content);
-        else
+        } else
             content += char;
     }
-    console.log(scanLine + ":" + scanIdxInLine + "종결되지 않은 문자열 리터럴입니다.");
+    tokenizerError("종결되지 않은 문자열 리터럴입니다.");
     return null;
 }
 
 function whiteSpaceTokenizer(sourceCode) {
     let content = "";
-    let char;
-    while (scanIdx < sourceCode.length) {
-        char = sourceCode[scanIdx];
-        if (char === " " || char === "\t" || char === "\r") {
-            content += char;
-            scanIdx++;
-            scanIdxInLine++;
-        } else if (char === "\n"){
-            scanIdx++;
-            scanIdxInLine = 0;
-            scanLine++;
-        }
-        else {
-            scanIdx--;
-            scanIdxInLine--;
-            break;
-        }
+    let char = sourceCode[scanIdx];
+    content += char;
+    nextIdx();
+    if (char == "\n"){
+        scanIdxInLine = 0;
+        scanLine++;
     }
     return new Token(Kind.WhiteSpace, content);
 }
 
 function identifierTokenizer(sourceCode) {
     let content = "";
-    let char = sourceCode[scanIdx];
 
-    while (scanIdx < sourceCode.length) {
-        char = sourceCode[scanIdx];
-        if (/[a-zA-Z0-9_]/.test(char)) {
-            content += char;
-            scanIdx++;
-            scanIdxInLine++;
-        } else {
-            scanIdx--;
-            scanIdxInLine--;
-            break;
+    while (scanIdx < sourceCode.length && isIdentifierPart(sourceCode[scanIdx])) {
+        content += sourceCode[scanIdx];
+        nextIdx();
+    }
+
+    if (isKeyword(content)) {
+        return new Token(Kind.Keyword, content);
+    }
+
+    return new Token(Kind.Identifier, content);
+}
+
+function isIdentifierPart(char) {
+    return /[a-zA-Z0-9_ㄱ-ㅎ가-힣]/.test(char);
+}
+
+function isKeyword(word) {
+    const keywords = ["rule", "if", "when", "then", "return", "true", "false"];
+    return keywords.includes(word);
+}
+
+function operatorTokenizer(sourceCode) {
+    let char = sourceCode[scanIdx];
+    let content = char;
+
+    nextIdx();
+    if (scanIdx + 1 < sourceCode.length) {
+        const nextChar = sourceCode[scanIdx];
+        if (/==|!=|<=|>=/.test(char + nextChar)) {
+            content += nextChar;
+            nextIdx();
         }
     }
-    return new Token(Kind.Identifier, content);
+    return new Token(Kind.OperatorAndPunctuator, content);
+}
+
+function numberTokenizer(sourceCode) {
+    let content = 0;
+
+    while (scanIdx < sourceCode.length && isDigit(sourceCode[scanIdx])) {
+        content = content * 10 + parseInt(sourceCode[scanIdx]);
+        nextIdx();
+        console.log(sourceCode[scanIdx], content);
+    }
+
+    if (sourceCode[scanIdx] == ".") {
+        nextIdx();
+        if (!isDigit(sourceCode[scanIdx])){
+            tokenizerError("종결되지 않은 소수 리터럴입니다.");
+            return null;
+        }
+        let e = 0.1;
+        while (scanIdx < sourceCode.length && isDigit(sourceCode[scanIdx])) {
+            content = content + e * parseInt(sourceCode[scanIdx]);
+            nextIdx();
+            e *= 0.1;
+        }
+    }
+
+    if (sourceCode[scanIdx] == "e" || sourceCode[scanIdx] == "E") {
+        nextIdx();
+        console.log(sourceCode[scanIdx]);
+        if(isDigit(sourceCode[scanIdx]) || "-+".includes(sourceCode[scanIdx])){
+            let d = 10;
+            if (sourceCode[scanIdx] == "-")
+                d = 0.1;
+            if (!isDigit(sourceCode[scanIdx])){
+                nextIdx();
+            }
+            let tmp = 0;
+            while (scanIdx < sourceCode.length && isDigit(sourceCode[scanIdx])) {
+                tmp = tmp * 10 + parseInt(sourceCode[scanIdx]);
+                nextIdx();
+            }
+            content *= Math.pow(d, tmp);
+        } else{
+            tokenizerError("지수 표현에서 숫자가 필요합니다.");
+            return null;
+        }
+    }
+    return new Token(Kind.NumberLiteral, content);
 }
